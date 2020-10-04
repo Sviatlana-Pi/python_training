6e99cf7b25ae8bf000a89ebbeb82f085ba0e10ea
Sviatlana Pinchuk <sviatlana.pinchuk@fortnox.se>
2020-10-04 22:22:02 +0200

    added some missing files to the fixture

---
+++ model/contact.py
@@ -0,0 +1,28 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, email2, homepage, email3, bday, bmonth, byear, new_group, notes, phone2, address2, ayear, amonth, aday):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.homepage = homepage
+        self.email3 = email3
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.new_group = new_group
+        self.notes = notes
+        self.phone2 = phone2
+        self.address2 = address2
+        self.ayear = ayear
+        self.amonth = amonth
+        self.aday = aday
